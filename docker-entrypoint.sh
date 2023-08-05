2d7b834a3f3de363e342e04684dff0e300fac5a6
SaintWe <93098501+SaintWe@users.noreply.github.com>
2023-08-06 06:43:16 +0800

    fix(docker-entrypoint.sh): 修复判断

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,21 @@ set -e
 
 cp /git/Caddyfile /etc/caddy/Caddyfile
 
-if [ -n $DOMAIN ]; then
+if [ $DOMAIN ]; then
     rm -rf /git/public
     cp -r /git/dist /git/public
     sed -i "s|https://sub.store|${DOMAIN}|g" `grep https://sub.store -rl /git/public`
     sed -i "s|##____||g" /etc/caddy/Caddyfile
 fi
 
-if [ -n $BEARER_TOKEN ]; then
+if [ $BEARER_TOKEN ]; then
     sed -i "s|__api_handle__|@api_auth|g" /etc/caddy/Caddyfile
     sed -i "s|__Bearer_Token__|${BEARER_TOKEN}|g" /etc/caddy/Caddyfile
 else
     sed -i "s|__api_handle__|@api_not_auth|g" /etc/caddy/Caddyfile
 fi
 
-if [ -n $D_TOKEN ]; then
+if [ $D_TOKEN ]; then
     sed -i "s|__download_handle__|@download_auth|g" /etc/caddy/Caddyfile
     sed -i "s|__Download_Token__|${D_TOKEN}|g" /etc/caddy/Caddyfile
 else
